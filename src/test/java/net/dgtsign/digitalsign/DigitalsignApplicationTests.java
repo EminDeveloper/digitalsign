28411341804150d981bf7b361bcc522004333f36
Andrey <emin.f.mamedov@gmail.com>
2021-12-05 14:42:03 +0400

    sign

---
+++ src/test/java/net/dgtsign/digitalsign/DigitalsignApplicationTests.java
@@ -0,0 +1,13 @@
+package net.dgtsign.digitalsign;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitalsignApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
